6376d7f5e64bbc66df88abec397015cf097a83a0
Shepico <Shepico@gmail.com>
2018-09-17 15:56:57 +0400

    Создан Box

--- src/lesson1/taskFruit/Fruit.java
+++ src/lesson1/taskFruit/Fruit.java
@@ -2,10 +2,14 @@ package lesson1.taskFruit;
 
 public class Fruit {
     private String sort;
-    private int weight;
+    private double weight;
 
     Fruit (String sort, int weight){
         this.sort = sort;
         this.weight = weight;
+    }
+
+    double getWeight() {
+        return this.weight;
     }
 }
